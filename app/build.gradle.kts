a650756c31f58b128e6df3f7c84d81930c8fbe53
Андрей Посессор <dronkrasava@MacBook-Pro-Andrej.local>
2025-10-15 15:35:57 +0300

    Компиляция и сборка проекта

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
     id("com.github.ben-manes.versions") version "0.53.0"
 }
 
@@ -14,6 +15,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 tasks.test {
